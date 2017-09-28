9ac2dcc68b741e35671221033d4b75e2f828628d
MiyamotoAkira <yefo.akira@gmail.com>
2017-09-28 17:23:54 +0100

    Eliminated intermediate variables
    This is following what probably will become one of the changes to the
    original Functional Calisthenics rules. Intermediate variables
    shouldn't be used, to force you to look into how you can change your
    code to do things in a more functional way

--- katas/marsrover/clojure/test/marsrover/core_test.clj
+++ katas/marsrover/clojure/test/marsrover/core_test.clj
@@ -4,6 +4,10 @@
 
 (def world (initialize-coordinate-system "10,10,standard-coordinates"))
 
+(defn get-rover [x y direction]
+  (-> (initialize-rover)
+      (place-rover-in-world world x y direction)))
+
 (deftest world-setup
   (testing "set initial size and coordinate system"
     (let [world (initialize-coordinate-system "5,6,standard-coordinates")]
@@ -13,89 +17,65 @@
 
 (deftest rover-in-world
   (testing "Put rover in world at 0,0,N"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 0 :N)]
-      (is (= 0 (:x (rover-position rover))))
-      (is (= 0 (:y (rover-position rover))))
-      (is (= :N (:direction (rover-position rover))))))
+    (is (= 0 (:x (rover-position (get-rover 0 0 :N)))))
+    (is (= 0 (:y (rover-position (get-rover 0 0 :N)))))
+    (is (= :N (:direction (rover-position (get-rover 0 0 :N))))))
 
   (testing "Put rover in world at 1,2,E"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 1 2 :E)]
-      (is (= 1 (:x (rover-position rover))))
-      (is (= 2 (:y (rover-position rover))))
-      (is (= :E (:direction (rover-position rover)))))))
+    (is (= 1 (:x (rover-position (get-rover 1 2 :E)))))
+    (is (= 2 (:y (rover-position (get-rover 1 2 :E)))))
+    (is (= :E (:direction (rover-position (get-rover 1 2 :E)))))))
 
 (deftest mars-rover-rotating
   (testing "turn right"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 0 :N)]
-      (is (= :E (:direction (rover-position (turn rover "R")))))
-      (is (= :S (:direction (rover-position (turn rover "RR")))))
-      (is (= :W (:direction (rover-position (turn rover "RRR")))))
-      (is (= :N (:direction (rover-position (turn rover "RRRR")))))))
+    (is (= :E (:direction (rover-position (turn (get-rover 0 0 :N) "R")))))
+    (is (= :S (:direction (rover-position (turn (get-rover 0 0 :N) "RR")))))
+    (is (= :W (:direction (rover-position (turn (get-rover 0 0 :N) "RRR")))))
+    (is (= :N (:direction (rover-position (turn (get-rover 0 0 :N) "RRRR"))))))
   
   (testing "turn left"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 0 :N)]
-      (is (= :W (:direction (rover-position (turn rover "L")))))
-      (is (= :S (:direction (rover-position (turn rover "LL")))))
-      (is (= :E (:direction (rover-position (turn rover "LLL")))))
-      (is (= :N (:direction (rover-position (turn rover "LLLL"))))))))
+    (is (= :W (:direction (rover-position (turn (get-rover 0 0 :N) "L")))))
+    (is (= :S (:direction (rover-position (turn (get-rover 0 0 :N) "LL")))))
+    (is (= :E (:direction (rover-position (turn (get-rover 0 0 :N) "LLL")))))
+    (is (= :N (:direction (rover-position (turn (get-rover 0 0 :N) "LLLL")))))))
 
 (deftest mars-moving
   (testing "move forwards North"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 0 :N)]
-      (is (= 1 (:x (move rover "F"))))
-      (is (= 2 (:x (move rover "FF"))))
-      (is (= 3 (:x (move rover "FFF"))))))
+    (is (= 1 (:x (move (get-rover 0 0 :N) "F"))))
+    (is (= 2 (:x (move (get-rover 0 0 :N) "FF"))))
+    (is (= 3 (:x (move (get-rover 0 0 :N) "FFF")))))
 
   (testing "move backwards North"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 3 0 :N)]
-      (is (= 2 (:x (move rover "B"))))
-      (is (= 1 (:x (move rover "BB"))))
-      (is (= 0 (:x (move rover "BBB"))))))
+    (is (= 2 (:x (move (get-rover 3 0 :N) "B"))))
+    (is (= 1 (:x (move (get-rover 3 0 :N) "BB"))))
+    (is (= 0 (:x (move (get-rover 3 0 :N) "BBB")))))
 
   (testing "move forwards South"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 3 0 :S)]
-      (is (= 2 (:x (move rover "F"))))
-      (is (= 1 (:x (move rover "FF"))))
-      (is (= 0 (:x (move rover "FFF"))))))
+    (is (= 2 (:x (move (get-rover 3 0 :S) "F"))))
+    (is (= 1 (:x (move (get-rover 3 0 :S) "FF"))))
+    (is (= 0 (:x (move (get-rover 3 0 :S) "FFF")))))
 
   (testing "move backwards South"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 0 :S)]
-      (is (= 1 (:x (move rover "B"))))
-      (is (= 2 (:x (move rover "BB"))))
-      (is (= 3 (:x (move rover "BBB"))))))
+    (is (= 1 (:x (move (get-rover 0 0 :S) "B"))))
+    (is (= 2 (:x (move (get-rover 0 0 :S) "BB"))))
+    (is (= 3 (:x (move (get-rover 0 0 :S) "BBB")))))
 
   (testing "move forwards East"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 0 :E)]
-      (is (= 1 (:y (move rover "F"))))
-      (is (= 2 (:y (move rover "FF"))))
-      (is (= 3 (:y (move rover "FFF"))))))
+    (is (= 1 (:y (move (get-rover 0 0 :E) "F"))))
+    (is (= 2 (:y (move (get-rover 0 0 :E) "FF"))))
+    (is (= 3 (:y (move (get-rover 0 0 :E) "FFF")))))
 
   (testing "move backwards East"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 3 :E)]
-      (is (= 2 (:y (move rover "B"))))
-      (is (= 1 (:y (move rover "BB"))))
-      (is (= 0 (:y (move rover "BBB"))))))
+    (is (= 2 (:y (move (get-rover 0 3 :E) "B"))))
+    (is (= 1 (:y (move (get-rover 0 3 :E) "BB"))))
+    (is (= 0 (:y (move (get-rover 0 3 :E) "BBB")))))
 
   (testing "move forwards West"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 3 :W)]
-      (is (= 2 (:y (move rover "F"))))
-      (is (= 1 (:y (move rover "FF"))))
-      (is (= 0 (:y (move rover "FFF"))))))
+    (is (= 2 (:y (move (get-rover 0 3 :W) "F"))))
+    (is (= 1 (:y (move (get-rover 0 3 :W) "FF"))))
+    (is (= 0 (:y (move (get-rover 0 3 :W) "FFF")))))
 
   (testing "move backwards West"
-    (let [rover (initialize-rover)
-          rover (place-rover-in-world rover world 0 0 :W)]
-      (is (= 1 (:y (move rover "B"))))
-      (is (= 2 (:y (move rover "BB"))))
-      (is (= 3 (:y (move rover "BBB")))))))
+    (is (= 1 (:y (move (get-rover 0 0 :W) "B"))))
+    (is (= 2 (:y (move (get-rover 0 0 :W) "BB"))))
+    (is (= 3 (:y (move (get-rover 0 0 :W) "BBB"))))))
