f6db67fe372f37d6d1d613b94dc8ded82d23f738
MiyamotoAkira <yefo.akira@gmail.com>
2017-10-19 08:31:13 +0100

    Refactored names
    Added a constant to eliminate on magic number. Also renamed a function
    to follow closer Elixir's convention (functions that return a boolean
    have a interrogation mark at the end)

--- katas/tennis/elixir/lib/tennis.ex
+++ katas/tennis/elixir/lib/tennis.ex
@@ -1,17 +1,9 @@
 defmodule Tennis do
   @moduledoc """
-  Documentation for Tennis.
+  Simple module library to score a single game of tennis
   """
+  @min_points_to_win 4
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tennis.hello
-      :world
-
-  """
   def create_game do
     %{player1: 0, player2: 0}
   end
@@ -24,11 +16,11 @@ defmodule Tennis do
     %{game | player => game[player] + 1}
   end
 
-  def check_player(game, player_to_win, other_player) do
-    (game[player_to_win] == 4
+  def has_player_won?(game, player_to_win, other_player) do
+    (game[player_to_win] == @min_points_to_win
       and game[player_to_win] > game[other_player])
     or
-    (game[player_to_win] > 4
+    (game[player_to_win] > @min_points_to_win
       and (game[player_to_win] > game[other_player] + 1))
   end
 
@@ -40,12 +32,13 @@ defmodule Tennis do
       3 -> "forty"
     end
   end
+
   def report_score(game) do
     case {game[:player1], game[:player2]} do
       {0, 0} -> "love all"
       {a, a} when a < 3 -> "#{score_as_string(a)} equals"
       {a, a} -> "deuce"
-      {a, b} when a < 4 and b < 4 -> "#{score_as_string(a)} #{score_as_string(b)}"
+      {a, b} when a < @min_points_to_win and b < @min_points_to_win -> "#{score_as_string(a)} #{score_as_string(b)}"
       {a, b} when (a - b) == 1 -> "advantage player 1"
       {a, b} when (b - a) == 1 -> "advantage player 2"
       _ -> "game finished"
@@ -54,9 +47,9 @@ defmodule Tennis do
 
   def get_winner(game) do
     cond do
-      check_player(game, :player1, :player2) -> :player1
-      check_player(game, :player2, :player1) -> :player2
-      true ->  :none
+      has_player_won?(game, :player1, :player2) -> :player1
+      has_player_won?(game, :player2, :player1) -> :player2
+      true -> :none
     end
   end
 end
